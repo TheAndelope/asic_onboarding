84a290ae22f0e3d9a8d5336699461808ff26ca12
Andy Duong <164644527+TheAndelope@users.noreply.github.com>
2026-03-08 13:16:59 -0400

    1.0.1 small fix

--- src/project.v
+++ src/project.v
@@ -34,7 +34,7 @@ module tt_um_uwasic_onboarding_andy_duong (
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-    .pwm_duty_cycle(pwm_duty_cycle),
+    .pwm_duty_cycle(pwm_duty_cycle)
   );
   
   pwm_peripheral pwm_peripheral_inst (
