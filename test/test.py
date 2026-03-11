26df10f506cabacfe6feb10f22ee79fcada3b73b
theandelope <theandelope16@gmail.com>
2026-03-11 16:37:36 -0400

    v2.0.0 tests complete

--- test/test.py
+++ test/test.py
@@ -4,6 +4,8 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
+from cocotb.triggers import FallingEdge
+from cocotb.triggers import Timer
 from cocotb.triggers import ClockCycles
 from cocotb.types import Logic
 from cocotb.types import LogicArray
@@ -151,7 +153,69 @@ async def test_spi(dut):
 
 @cocotb.test()
 async def test_pwm_freq(dut):
-    # Write your test here
+    dut._log.info("Start PWM Frequency test")
+
+    # Set the clock period to 100 ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut._log.info("Reset")
+    dut.ena.value = 1
+    ncs = 1
+    bit = 0
+    sclk = 0
+    dut.ui_in.value = ui_in_logicarray(ncs, bit, sclk)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 5)
+
+    await send_spi_transaction(dut, 1, 0x00, 0xFF)  # Write transaction
+    await send_spi_transaction(dut, 1, 0x02, 0xFF)  # Write transaction
+    await ClockCycles(dut.clk, 5000)
+
+    dut._log.info("Write transaction, address 0x04, data 0x80")
+    await send_spi_transaction(dut, 1, 0x04, 0x80)  # Write transaction
+    await ClockCycles(dut.clk, 5000)
+    
+    # sync to falling edge
+    pwm1 = 0
+    pwm2 = 0
+    for _ in range(100000):
+        await ClockCycles(dut.clk, 1)
+        pwm2 = pwm1
+        pwm1 = int(dut.uo_out.value[0])
+        if pwm2 == 1 and pwm1 == 0:
+            break
+    else:
+        assert False, f"Timeout syncing for value {i}"
+
+    pwm1 = 0
+    pwm2 = 0
+    t_falling = 0
+    t_rising1 = 0
+    t_rising2 = 0
+    while t_falling == 0 or t_rising1 == 0 or t_rising2 == 0:
+        await ClockCycles(dut.clk, 1)
+        pwm2 = pwm1
+        pwm1 = int(dut.uo_out.value[0])
+    
+        if pwm2 == 0 and pwm1 == 1 and t_rising1 == 0:
+            t_rising1 = cocotb.utils.get_sim_time("ns")
+        elif pwm2 == 0 and pwm1 == 1 and t_rising1 != 0:
+            t_rising2 = cocotb.utils.get_sim_time("ns")
+    
+        # only capture falling edge AFTER we have rising1
+        if pwm2 == 1 and pwm1 == 0 and t_rising1 != 0:
+            t_falling = cocotb.utils.get_sim_time("ns")
+
+    period = t_rising2 - t_rising1
+    frequency = 1e9 / period
+    
+    assert abs(frequency - 3000) < 30
+    dut._log.info(frequency)
+
     dut._log.info("PWM Frequency test completed successfully")
 
 
@@ -169,23 +233,72 @@ async def test_pwm_duty(dut):
     ncs = 1
     bit = 0
     sclk = 0
-    t_falling
     dut.ui_in.value = ui_in_logicarray(ncs, bit, sclk)
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 5)
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 5)
-
-    dut._log.info("Write transaction, address 0x00, data 0xFF")
+    
     await send_spi_transaction(dut, 1, 0x00, 0xFF)  # Write transaction
-    await ClockCycles(dut.clk, 5000) 
-
-    dut._log.info("Write transaction, address 0x02, data 0xFF")
     await send_spi_transaction(dut, 1, 0x02, 0xFF)  # Write transaction
     await ClockCycles(dut.clk, 5000)
 
-    dut._log.info("Write transaction, address 0x04, data 0x00")
     await send_spi_transaction(dut, 1, 0x04, 0x00)  # Write transaction
     await ClockCycles(dut.clk, 5000)
+    
+    #test case for 0% duty cycle
+    for _ in range(20):
+        await ClockCycles(dut.clk, 3000)
+        assert int(dut.uo_out.value[0]) == 0
+    dut._log.info("pwm duty cycle test completed successfully")
+    
+    #test case for 100% duty cycle
+    await send_spi_transaction(dut, 1, 0x04, 0xFF)  # Write transaction
+    await ClockCycles(dut.clk, 5000)
 
-    dut._log.info("PWM Duty Cycle test completed successfully")
+    for _ in range(20):
+        await ClockCycles(dut.clk, 3000)
+        assert int(dut.uo_out.value[0]) == 1
+    
+    tests = [187, 215, 128, 1]
+    for i in tests:
+        await send_spi_transaction(dut, 1, 0x04, i)  # Write transaction
+        await ClockCycles(dut.clk, 3000)
+
+        # sync to falling edge
+        pwm1 = 0
+        pwm2 = 0
+        for _ in range(100000):
+            await ClockCycles(dut.clk, 1)
+            pwm2 = pwm1
+            pwm1 = int(dut.uo_out.value[0])
+            if pwm2 == 1 and pwm1 == 0:
+                break
+        else:
+            assert False, f"Timeout syncing for value {i}"
+
+        # measure falling/rising edge
+        pwm1 = 0
+        pwm2 = 0
+        t_falling = 0
+        t_rising1 = 0
+        t_rising2 = 0
+        while t_falling == 0 or t_rising1 == 0 or t_rising2 == 0:
+            await ClockCycles(dut.clk, 1)
+            pwm2 = pwm1
+            pwm1 = int(dut.uo_out.value[0])
+        
+            if pwm2 == 0 and pwm1 == 1 and t_rising1 == 0:
+                t_rising1 = cocotb.utils.get_sim_time("ns")
+            elif pwm2 == 0 and pwm1 == 1 and t_rising1 != 0:
+                t_rising2 = cocotb.utils.get_sim_time("ns")
+        
+            # only capture falling edge AFTER we have rising1
+            if pwm2 == 1 and pwm1 == 0 and t_rising1 != 0:
+                t_falling = cocotb.utils.get_sim_time("ns")
+
+        period = t_rising2 - t_rising1
+        high_time = t_falling - t_rising1
+        duty_cycle = (high_time / period) * 100
+        expected = (i / 255) * 100 
+        assert abs(duty_cycle - expected) < 1
