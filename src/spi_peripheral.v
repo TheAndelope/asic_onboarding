794d5188c5b1584a4d4cac037b142aa289484154
theandelope <theandelope16@gmail.com>
2026-03-11 23:20:33 -0400

    v3.0.3 little update

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -23,7 +23,7 @@ module spi_peripheral(
     reg ncs_sync2;
 
     reg [15:0] buffer;
-    reg [4:0] bit_count;
+    reg [5:0] bit_count;
 
     wire ncs_posedge = ~ncs_sync2 & ncs_sync1;
 
@@ -56,7 +56,7 @@ module spi_peripheral(
             bit_count <= 0;
         end else if (transaction_ready) begin
             transaction_ready <= 1'b0;
-        end else if (ncs_posedge && bit_count == 16) begin // end block (transaction ends)
+        end else if (ncs_posedge && bit_count == 5'd16) begin // end block (transaction ends)
             transaction_ready <= 1'b1;
             bit_count <= 0;
         end else if (ncs_sync2 == 1'b0) begin // if sending data
